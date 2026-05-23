059e02ac9b4d85c4fb536dd4668afdc419198e7a
snsb08 <76939844+snsb08@users.noreply.github.com>
2026-05-23 23:14:05 +0000

    Deploying to website from @ snsb08/wdd331-practice@f0051be0b51fd23e8961eaa4c072675e8c7f01c9 🚀

---
+++ unit-3/toggle.js
@@ -0,0 +1,18 @@
+const button = document.getElementById("theme-toggle");
+
+button.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  document.body.classList.add("dark");
+}
